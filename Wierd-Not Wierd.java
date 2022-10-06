4d45d058ba8c7bd98f764e886e371ee1e24e411e
JobinRufus <114924946+JobinRufus@users.noreply.github.com>
2022-10-06 16:49:09 +0530

    Update Wierd-Not Wierd.java

--- Wierd-Not Wierd.java
+++ Wierd-Not Wierd.java
@@ -6,28 +6,33 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+ public class ifelse {
 
+        public static void main(String[] args) {
 
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        scanner.close();
-         if (N%2 != 0 )
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();            
+            String ans="";
+	    if(n<=100)
+	    {
+            if(n%2==1){
+              ans = "Weird";
+            }
+            else {
+                if (n >= 2 && n <= 5) {
+                    ans = "Not Weird";
+                } else if (n >= 6 && n <= 20) {
+                    ans = "Weird";
+                } else {
+                    ans = "Not Weird";
+                }
             
-            System.out.println("Weird");
-            
-
-                if (N%2 == 0 )
-                
-                    if(N>=2 && N<=5 || N>20)
-                    System.out.println("Not Weird");
-
-                    else if(N>=6 && N<=20)
-                    System.out.println("Weird");
+               //Complete the code                
+            }
+            System.out.println(ans);
+	   }  
+	   else {
+	   System.out.println("The value is not found");           
+          }
+        }
     }
-}
